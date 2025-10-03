9d150e1d53eb2005cd886e1c881be6fc493b5230
Bant3r <bobby36ko@outlook.com>
2025-10-03 14:34:40 -0400

    Update Happy.lua

--- Happy.lua
+++ Happy.lua
@@ -1,1 +1,32 @@
+if game.PlaceId == 17890861313 then
+    local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+    local Window = OrionLib:MakeWindow({
+        Name = "ABI │ Happy",
+        HidePremium = false,
+        IntroEnabled = false,
+        IntroText = "ABI",
+        SaveConfig = true,
+        ConfigFolder = "XlurConfig"
+    })
 
+    local MiscTab = Window:MakeTab({
+        Name = "Misc",
+        Icon = "rbxassetid://4299432428", -- You can change this icon if you'd like
+        PremiumOnly = false
+    })
+
+    _G.ESP = false
+
+    MiscTab:AddToggle({
+        Name = "ESP",
+        Default = false,
+        Callback = function(state)
+            _G.ESP = state
+            if state then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Bant3r241/CornerESP/refs/heads/main/ESP.lua"))()
+            end
+        end
+    })
+
+    OrionLib:Init()
+end
