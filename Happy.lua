d0c423c304673d1d3a1504b476c8e7284e61baf2
Bant3r <bobby36ko@outlook.com>
2025-10-04 12:53:34 -0400

    Update Happy.lua

--- Happy.lua
+++ Happy.lua
@@ -1,31 +1,30 @@
-if game.PlaceId == 17890861313 then
-    local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
-    local Window = OrionLib:MakeWindow({
-        Name = "ABI │ Happy",
-        HidePremium = false,
-        IntroEnabled = false,
-        IntroText = "ABI",
-        SaveConfig = true,
-        ConfigFolder = "XlurConfig"
-    })
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+local Window = OrionLib:MakeWindow({
+    Name = "ABI │ Happy!",
+    HidePremium = false,
+    IntroEnabled = false,
+    IntroText = "ABI",
+    SaveConfig = true,
+    ConfigFolder = "XlurConfig"
+})
 
-    local MiscTab = Window:MakeTab({
-        Name = "Misc",
-        Icon = "rbxassetid://4299432428",
-        PremiumOnly = false
-    })
+local MiscTab = Window:MakeTab({
+    Name = "Misc",
+    Icon = "rbxassetid://4299432428",
+    PremiumOnly = false
+})
 
-    _G.ESP = false
+-- Global toggle variable for ESP
+_G.ESP = false
 
-    MiscTab:AddToggle({
-        Name = "ESP",
-        Default = false,
-        Callback = function(state)
-            _G.ESP = state
-            -- now load the ESP script each time; it will check _G.ESP
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Bant3r241/chams/refs/heads/main/ESP.lua"))()
-        end
-    })
+MiscTab:AddToggle({
+    Name = "ESP",
+    Default = false,
+    Callback = function(state)
+        _G.ESP = state
+        -- Load ESP logic; it will check _G.ESP and enable/disable accordingly
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Bant3r241/chams/refs/heads/main/ESP.lua"))()
+    end
+})
 
-    OrionLib:Init()
-end
+OrionLib:Init()
